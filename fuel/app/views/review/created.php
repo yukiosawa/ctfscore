af387b3e71650393acbb8cba9c1375a37ddce466
mage <m---@users.noreply.github.com>
2015-11-28 04:10:31 +0900

    レビューデザイン調整

--- fuel/app/views/review/created.php
+++ fuel/app/views/review/created.php
@@ -1,8 +1,6 @@
 <?php if (!empty($errmsg)): ?>
   <div class='alert alert-danger'><?php echo $errmsg ?></div>
 <?php else: ?>
-  <div class='alert alert-success'><?php echo $msg ?></div>
+  <div class="text-center"><?php echo Asset::img('thanksforfeedback.png', array('style' => 'width: 400px;')); ?></div>
   <?php echo render('review/_view', array('review' => $review)); ?>
 <?php endif; ?>
-
-
