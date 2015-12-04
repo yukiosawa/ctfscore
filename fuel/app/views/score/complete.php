1717aadd4666046f1944b6dd0370c9a74aaff5e8
mage <m---@users.noreply.github.com>
2015-12-04 21:25:33 +0900

    無限ループ

--- fuel/app/views/score/complete.php
+++ fuel/app/views/score/complete.php
@@ -1,6 +1,6 @@
 <?php
     if ($sound_on) {
-        printf('<audio src="%s" autoplay></audio>', $complete_sound);
+        printf('<audio src="%s" autoplay loop></audio>', $complete_sound);
     }
 ?>
 <div id="overlay" style="display: block; background-position: center center; background-image: url(<?php echo $complete_img; ?>);"></div>
